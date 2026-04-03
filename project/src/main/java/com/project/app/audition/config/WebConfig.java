45f3931719b2bf56e1c7d60066788cbb49ea6ef0
hjeei1128 <hjeei1128@naver.com>
2026-04-03 18:31:11 +0900

    모금페이지

--- project/src/main/java/com/project/app/audition/config/WebConfig.java
+++ project/src/main/java/com/project/app/audition/config/WebConfig.java
@@ -27,6 +27,12 @@ public class WebConfig implements WebMvcConfigurer {
         
         // 리액트 호출 주소: /images/파일명.jpg
         registry.addResourceHandler("/images/**")
+       // .addResourceLocations("file:///C:/upload/action profile/");
+        .addResourceLocations("file:///C:/upload/action profile/images/");
+        
+        
+     // 추가: /media/파일명.jpg 호출 시 실제 images 폴더 안을 뒤짐
+        registry.addResourceHandler("/media/**")
                 .addResourceLocations("file:///C:/upload/action profile/images/");
     }
     
