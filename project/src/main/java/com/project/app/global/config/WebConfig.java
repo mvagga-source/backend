bd62878ca51e117f3310d65b8ec126db1154d458
jhzong <jhzong05@gmail.com>
2026-04-09 18:26:49 +0900

    이미지 리소스 핸들러 정리 + Config 파일 구조 정리

---
+++ project/src/main/java/com/project/app/global/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.project.app.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	 @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+	        // 대표 프로필 이미지
+	        // 호출: /profile/파일명.jpg
+	        registry.addResourceHandler("/profile/**")
+	                .addResourceLocations("file:///C:/upload/action profile/");
+
+	        // 상세 프로필 사진
+	        // 호출: /images/파일명.jpg
+	        registry.addResourceHandler("/images/**")
+	                .addResourceLocations("file:///C:/upload/action profile/images/");
+
+	        // 팀 대표 이미지
+	        // 호출: /teamImages/파일명.jpg
+	        registry.addResourceHandler("/teamImages/**")
+	                .addResourceLocations("file:///C:/upload/action profile/teamImages/");
+
+	        // 전체 업로드 폴더 직접 접근 (MyVote 등)
+	        // 호출: /upload/파일명
+	        registry.addResourceHandler("/upload/**")
+	                .addResourceLocations("file:///C:/upload/");
+	    }
+}
